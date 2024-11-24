ceb9a82038445326a182622da70342ea4d8bf510
mariorodriguez255 <mario.rodriguez.2006@protonmail.com>
2024-11-24 19:58:07 +0100

    subo proyecto

---
+++ src/main/java/uax/proyectos/p1/Objetos4Application.java
@@ -0,0 +1,9 @@
+package uax.proyectos.p1;
+
+public class Objetos4Application {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/test/java/uax/practicas/pr7/Objetos4ApplicationTests.java
@@ -0,0 +1,13 @@
+package uax.practicas.pr7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Objetos4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
